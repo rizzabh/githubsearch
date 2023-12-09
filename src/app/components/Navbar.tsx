33c9b7a1b4abdf398b94cbce08d5493b98db7355
rizzabh <pandeyrishabh966@gmail.com>
2023-12-09 23:41:57 +0530

    msg

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     </div>
     {modal && <div className="modal p-4 bg-gray-800 left-1/4 rounded-md border shadow-2xl backdrop-blur-lg bg-opacity-40 border-gray-700 z-10 w-6/12 m-auto absolute h-80">
       <div className="flex justify-between"><div className="text-2xl font-semibold">Search History</div><div className="bg-gray-600 bg-opacity-20 hover:bg-gray-700 border border-gray-600 px-2 py-1 rounded-lg cursor-pointer h-fit w-fit" onClick={toggleModal}>close</div></div>
-      <div className="text-gray-400">Working on this sorry, Learning about Local storage and Session Storage. I will implement this as soon as im free</div>
+      <div className="text-gray-400">Learning about Local storage and Session Storage. I will implement this as soon as Possible</div>
     </div>
 
 
